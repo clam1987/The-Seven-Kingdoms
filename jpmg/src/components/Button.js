0a5afc490ebb509cfbbd092f334f5e7c63be8ed4
eunhyegina <eunhyegina@gmail.com>
2019-05-14 13:16:42 -0700

    button changes

--- jpmg/src/components/Button.js
+++ jpmg/src/components/Button.js
@@ -4,9 +4,6 @@ const Button = props => (
  
 <div className="group">
 <div className="round-button"><div class="round-button-circle"><a href="http://example.com" class="round-button">SHOP</a></div></div>
-<div className="round-button"><div class="round-button-circle"><a href="http://example.com" class="round-button">STATS</a></div></div>
-<div className="round-button"><div class="round-button-circle"><a href="http://example.com" class="round-button">QUESTS</a></div></div>
-<div className="round-button"><div class="round-button-circle"><a href="http://example.com" class="round-button">INVENTORY</a></div></div>
 </div>
 
 
