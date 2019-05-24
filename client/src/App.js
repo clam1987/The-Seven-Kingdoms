cc16cb2776fecd0784d2ca6fda032a47595b189c
JDVapor <46424734+JDVapor@users.noreply.github.com>
2019-05-23 21:54:29 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Import Components
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
-import Battle from "./components/pages/Battle/Battle";
-import Character from "./components/pages/Character/Character";
-import Inventory from "./components/pages/Inventory/Inventory";
-import Login from "./components/pages/Login/Login";
-import Signup from "./components/pages/Signup/Signup";
-import Quest from "./components/pages/Quest/Quest";
-import Shop from "./components/pages/Shop/Shop";
-import Stats from "./components/pages/Stats/Stats";
-import Town from "./components/pages/Town/Town";
-import Enter from "./components/pages/Enter/Enter";
-import PrivateRoute from "./components/pages/PrivateRoute/PrivateRoute"
+import Battle from "./pages/Battle/Battle";
+import Character from "./pages/Character/Character";
+import Inventory from "./pages/Inventory/Inventory";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Quest from "./pages/Quest/Quest";
+import Shop from "./pages/Shop/Shop";
+import Stats from "./pages/Stats/Stats";
+import Town from "./pages/Town/Town";
+import Enter from "./pages/Enter/Enter";
+import PrivateRoute from "./pages/PrivateRoute/PrivateRoute"
 
 import './App.css';
 
@@ -35,6 +35,19 @@ class App extends Component {
       <Router>
         <div className="App">
           <Nav />
+
+          <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/battle" component={Battle} />
+          <Route exact path="/character" component={Character} />
+          <Route exact path="/enter" component={Enter} />
+          <Route exact path="/inventory" component={Inventory} />
+          <Route exact path="/quest" component={Quest} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/stats" component={Stats} />
+          <Route exact path="/town" component={Town} />
+
+      {/*
           <Switch>
           <Route exact path="/" render={props => <Login {...props} setUserData={this.setUserData}/>} />
           <Route exact path="/signup" component={Signup} />
@@ -46,8 +59,8 @@ class App extends Component {
           <Route exact path="/protected/shop" render={(props) => this.state.user ? <Shop {...props}/> : <Login {...props} setUserData={this.setUserData}/> }/>
           <Route exact path="/protected/stats" render={(props) => this.state.user ? <Stats {...props}/> : <Login {...props} setUserData={this.setUserData}/> }/>
           <Route exact path="/protected/enter" render={(props) => this.state.user ? <Enter {...props}/> : <Login {...props} setUserData={this.setUserData}/> }/>
-          </Switch>  
-
+          </Switch>*/}
+       
           <Footer />
         </div>
       </Router>
